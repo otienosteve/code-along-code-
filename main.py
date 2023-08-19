f59ee25fa1e6b8bda719605280ca4680d813a9d0
otienosteve <steveotieno701@gmail.com>
2023-08-19 10:26:43 +0300

    Add Endpint for Single Get

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from models import session, Student
 
 app = FastAPI()
 
 @app.get('/')
 def index():
     return {'msg' : 'Welcome to fastapi'}
+
+
+@app.get('/students')
+def get_all_students():
+    #logic to query data from the database and return results to the user /client
+    students = session.query(Student).all()
+    return students
+
+@app.get('/students/{id}')
+def get_one_student(id: int):
+    #logic to query data for a single student from the database and return results to the user/ client
+    return {'msg':f'student {id}'} 
